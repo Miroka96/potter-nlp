f3dbc0e09824128cf955649933164d351c84705d
Mirko Krause <mirko.krause@student.hpi.de>
2017-12-14 19:14:52 +0100

    cleaned up code and created pycharm project

--- lstm/recurrent_keras.py
+++ lstm/recurrent_keras.py
@@ -1,77 +1,100 @@
 from __future__ import print_function
-import matplotlib.pyplot as plt
-import numpy as np
-import time
-import csv
+
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM, SimpleRNN
 from keras.layers.wrappers import TimeDistributed
 import argparse
-from RNN_utils import *
+try:
+    from lstm.RNN_utils import *
+except:
+    # for direct execution
+    from RNN_utils import *
 
-# Parsing arguments for Network definition
-ap = argparse.ArgumentParser()
-ap.add_argument('-data_dir', default='../final_data/allBooks.txt')
-ap.add_argument('-batch_size', type=int, default=50)
-ap.add_argument('-layer_num', type=int, default=2)
-ap.add_argument('-seq_length', type=int, default=50)
-ap.add_argument('-hidden_dim', type=int, default=500)
-ap.add_argument('-generate_length', type=int, default=500)
-ap.add_argument('-nb_epoch', type=int, default=20)
-ap.add_argument('-mode', default='train')
-ap.add_argument('-weights', default='')
-args = vars(ap.parse_args())
+
+def parse_arguments():
+    # Parsing arguments for Network definition
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-data_dir', default='../final_data/allBooks.txt')
+    ap.add_argument('-batch_size', type=int, default=50)
+    ap.add_argument('-layer_num', type=int, default=2)
+    ap.add_argument('-seq_length', type=int, default=50)
+    ap.add_argument('-hidden_dim', type=int, default=500)
+    ap.add_argument('-generate_length', type=int, default=200)
+    ap.add_argument('-nb_epoch', type=int, default=20)
+    ap.add_argument('-mode', default='train')
+    ap.add_argument('-weights', default='')
+    ap.add_argument('-epochs_per_iteration', type=int, default=10)
+    args = vars(ap.parse_args())
+    return args
+
+
+args = parse_arguments()
 
 DATA_DIR = args['data_dir']
 BATCH_SIZE = args['batch_size']
-HIDDEN_DIM = args['hidden_dim']
-SEQ_LENGTH = args['seq_length']
-WEIGHTS = args['weights']
-
-GENERATE_LENGTH = args['generate_length']
 LAYER_NUM = args['layer_num']
+SEQ_LENGTH = args['seq_length']
+HIDDEN_DIM = args['hidden_dim']
+GENERATE_LENGTH = args['generate_length']
+NB_EPOCH = args['nb_epoch']
+MODE = args['mode']
+WEIGHTS = args['weights']
+EPI = args['epochs_per_iteration']
 
 # Creating training data
-X, y, VOCAB_SIZE, ix_to_char = load_data(DATA_DIR, SEQ_LENGTH)
+X, y, ix_to_char = load_data(DATA_DIR, SEQ_LENGTH)
+VOCAB_SIZE = len(ix_to_char)
 
-# Creating and compiling the Network
-model = Sequential()
-model.add(LSTM(HIDDEN_DIM, input_shape=(None, VOCAB_SIZE), return_sequences=True))
-for i in range(LAYER_NUM - 1):
-  model.add(LSTM(HIDDEN_DIM, return_sequences=True))
-model.add(TimeDistributed(Dense(VOCAB_SIZE)))
-model.add(Activation('softmax'))
-model.compile(loss="categorical_crossentropy", optimizer="rmsprop")
+
+def create_model():
+    # Creating and compiling the Network
+    model = Sequential()
+    model.add(LSTM(HIDDEN_DIM, input_shape=(None, VOCAB_SIZE), return_sequences=True))
+    for i in range(LAYER_NUM - 1):
+        model.add(LSTM(HIDDEN_DIM, return_sequences=True))
+    model.add(TimeDistributed(Dense(VOCAB_SIZE)))
+    model.add(Activation('softmax'))
+    model.compile(loss="categorical_crossentropy", optimizer="rmsprop")
+    return model
+
+
+model = create_model()
 
 # Generate some sample before training to know how bad it is!
-generate_text(model, args['generate_length'], VOCAB_SIZE, ix_to_char)
+generate_text(model, GENERATE_LENGTH, ix_to_char)
 
 if not WEIGHTS == '':
-  model.load_weights(WEIGHTS)
-  nb_epoch = int(WEIGHTS[WEIGHTS.rfind('_') + 1:WEIGHTS.find('.')])
+    # Loading the trained weights
+    model.load_weights(WEIGHTS)
+    nb_epoch = int(WEIGHTS[WEIGHTS.rfind('_') + 1:WEIGHTS.find('.')])
+    print('Loaded weights for epoch {}'.format(nb_epoch))
 else:
-  nb_epoch = 0
+    nb_epoch = 0
 
-epochs_per_iteration = 10
+if MODE == 'train' or WEIGHTS == '':
+    while True:
+        print('\n\nEpoch: {}\n'.format(nb_epoch))
+        try:
+            model.fit(X, y, batch_size=BATCH_SIZE, verbose=1, epochs=EPI)
+        except:
+            print('Encountered an Error while training this epoch')
+            continue
 
-# Training if there is no trained weights specified
-if args['mode'] == 'train' or WEIGHTS == '':
-  while True:
-    print('\n\nEpoch: {}\n'.format(nb_epoch))
-    model.fit(X, y, batch_size=BATCH_SIZE, verbose=1, epochs=epochs_per_iteration)
-    nb_epoch += epochs_per_iteration
-    txt = generate_text(model, GENERATE_LENGTH, VOCAB_SIZE, ix_to_char)
-    with open('generated_' + str(nb_epoch), 'w') as out:
-    	out.write(txt)
-    # if nb_epoch % 10 == 0:
-    model.save_weights('checkpoint_layer_{}_hidden_{}_epoch_{}.hdf5'.format(LAYER_NUM, HIDDEN_DIM, nb_epoch))
+        nb_epoch += EPI
+        txt = generate_text(model, GENERATE_LENGTH, ix_to_char)
+        with open('generated_' + str(nb_epoch), 'w') as out:
+            out.write(txt)
+        model.save_weights('checkpoint_layer_{}_hidden_{}_epoch_{}.hdf5'.format(LAYER_NUM, HIDDEN_DIM, nb_epoch))
 
-# Else, loading the trained weights and performing generation only
-elif WEIGHTS == '':
-  # Loading the trained weights
-  model.load_weights(WEIGHTS)
-  generate_text(model, GENERATE_LENGTH, VOCAB_SIZE, ix_to_char)
-  print('\n\n')
+# performing generation only
+elif MODE == 'test':
+    if WEIGHTS == '':
+        print('Specify saved Weights!')
+        exit(1)
+
+    generate_text(model, GENERATE_LENGTH, ix_to_char)
+    print('\n\n')
+
 else:
-  print('\n\nNothing to do!')
+    print('\n\nNothing to do!')
